b577b30edb9f4c2df81f4c50bb3d6478f824daed
akyghodake7769 <156925683+akyghodake7769@users.noreply.github.com>
2026-02-10 08:01:18 +0530

    Update solution.py

--- QP-201/QP-201/gym_memership_usage_analytics_pandas/student_workspace/solution.py
+++ QP-201/QP-201/gym_memership_usage_analytics_pandas/student_workspace/solution.py
@@ -6,24 +6,24 @@ class GymUsageAnalyzer:
         return pd.DataFrame(logs, columns=["Member", "Branch", "Workout", "Duration"])
 
     def compute_total_duration(self, usage_df: pd.DataFrame) -> pd.DataFrame:
-        result = usage_df.groupby("Member")["Duration"].sum().reset_index()
-        result.rename(columns={"Duration": "Total Duration"}, inplace=True)
+        #your_code_here
         return result
 
     def add_calorie_column(self, usage_df: pd.DataFrame) -> pd.DataFrame:
-        df = usage_df.copy()
-        # Define burn rates
         rates = {"Cardio": 8, "Weights": 7, "Yoga": 5}
         # Map workout type to burn rate; default to 6 if not found
-        df["Rate"] = df["Workout"].map(rates).fillna(6)
-        df["Calories Burned"] = (df["Duration"] * df["Rate"]).astype(int)
+         #your_code_here
         return df.drop(columns=["Rate"])
 
     def filter_long_sessions(self, usage_df: pd.DataFrame, min_minutes: int) -> pd.DataFrame:
-        return usage_df[usage_df["Duration"] >= min_minutes].reset_index(drop=True)
+        #your_code_here
+        return 
 
     def get_top_sessions(self, usage_df: pd.DataFrame, n: int) -> pd.DataFrame:
-        return usage_df.sort_values(by="Calories Burned", ascending=False).head(n)
+        #your_code_here
+        return top_session_df
     
     def remove_duplicates(self, usage_df: pd.DataFrame) -> pd.DataFrame:
-        return usage_df.drop_duplicates().reset_index(drop=True)
+        #your_code_here
+        return unique_df
+
