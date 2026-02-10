de871788d534fcb16a05db3e17acc535fc0149ad
akyghodake7769 <156925683+akyghodake7769@users.noreply.github.com>
2026-02-10 08:46:45 +0530

    Update solution.py

--- QO-401/QO-401/supply_chain_inventory_tracker_oops/student_workspace/solution.py
+++ QO-401/QO-401/supply_chain_inventory_tracker_oops/student_workspace/solution.py
@@ -5,30 +5,20 @@ class SupplyChainInventory:
         self.stock = {}
 
     def add_product(self, product_id: str, quantity: int) -> dict:
-        """Add a new product or update existing product stock.        
-        Args: product_id: Unique product identifier, quantity: Number of units to add
-        Returns:    Updated stock dictionary """
+
         #your code here
         return self.stock
 
     def fulfill_order(self, product_id: str, quantity: int) -> dict:
-        """Fulfill an order by deducting from stock
-        Args: product_id: Unique product identifier, quantity: Number of units to deduct
-        Returns: Updated stock dictionary
-        Raises: ValueError: If product not found or insufficient stock """
         #your code here
         return self.stock
 
     def restock_return(self, product_id: str, quantity: int) -> dict:
-        """Add returned goods back into stock.
-        Args: product_id: Unique product identifier, quantity: Number of units to add back
-        Returns: Updated stock dictionary """
         #your code here        
         return self.stock
 
     def list_available_products(self) -> list:
-        """List all products with stock greater than 0
-        Returns: List of product IDs with available stock """
         #your code ehere
         return [product_id for product_id, qty in self.stock.items() if qty > 0]
+
 
