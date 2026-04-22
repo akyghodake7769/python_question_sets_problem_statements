c6d928a4ac41a76e47b4df7ffaa7cfcfed962c33
akyghodake7769 <156925683+akyghodake7769@users.noreply.github.com>
2026-04-22 22:57:46 +0530

    Add placeholders for Comparator methods
    Refactor Comparator class methods to include placeholders for implementation.

--- COD_J01/COD_J01/java_comparator/student_workspace/Solution.java
+++ COD_J01/COD_J01/java_comparator/student_workspace/Solution.java
@@ -1,46 +1,24 @@
-// // import java.util.Arrays;
+// student_workspace/solution.java
 
-// /* 
-//  * Implement the 'Comparator' class here.
-//  * It should have three overloaded 'compare' methods.
-//  * DO NOT use access modifiers (e.g.: 'public') in your class declarations.
-//  */
-
-// class Comparator {
-//     boolean compare(int a, int b) {
-//         // Write your code here
-//         return false;
-//     }
-
-//     boolean compare(String a, String b) {
-//         // Write your code here
-//         return false;
-//     }
-
-//     boolean compare(int[] a, int[] b) {
-//         // Write your code here
-//         return false;
-//     }
-// }
-
-// import java.util.Arrays;
+/* 
+ * Implement the 'Comparator' class here.
+ * It should have three overloaded 'compare' methods.
+ * DO NOT use access modifiers (e.g.: 'public') in your class declarations.
+ */
 
 class Comparator {
     boolean compare(int a, int b) {
-        return a == b;
+        // Write your code here
+        return false;
     }
 
     boolean compare(String a, String b) {
-        return a.equals(b);
+        // Write your code here
+        return false;
     }
 
     boolean compare(int[] a, int[] b) {
-        if (a.length != b.length)
-            return false;
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] != b[i])
-                return false;
-        }
-        return true;
+        // Write your code here
+        return false;
     }
 }
