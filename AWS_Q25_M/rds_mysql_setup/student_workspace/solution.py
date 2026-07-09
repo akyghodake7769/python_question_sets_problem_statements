24a1fc827c672bfba1fe70d2f9b07ffe850ae0d3
Nandini Jambhulkar <nandini.atwork1204@gmail.com>
2026-07-09 10:35:13 +0530

    Delete AWS_Q25_M directory

--- AWS_Q25_M/rds_mysql_setup/student_workspace/solution.py
+++
@@ -1,8 +0,0 @@
-# Starter code / sample solution for rds_mysql_setup
-
-def solve():
-    # Candidates configure resources through the AWS Console/CLI.
-    pass
-
-if __name__ == "__main__":
-    solve()
