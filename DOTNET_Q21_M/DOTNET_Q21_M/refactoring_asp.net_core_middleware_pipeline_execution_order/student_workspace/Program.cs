b2bcd872ee06e9858bd788813ac09b2d1c31a6e9
Nandini Jambhulkar <nandini.atwork1204@gmail.com>
2026-07-31 18:39:10 +0530

    Delete DOTNET_Q21_M/DOTNET_Q21_M/refactoring_asp.net_core_middleware_pipeline_execution_order directory

--- DOTNET_Q21_M/DOTNET_Q21_M/refactoring_asp.net_core_middleware_pipeline_execution_order/student_workspace/Program.cs
+++
@@ -1,9 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-// Incorrect pipeline order: static files served before authentication
-app.UseStaticFiles();
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.Run();
