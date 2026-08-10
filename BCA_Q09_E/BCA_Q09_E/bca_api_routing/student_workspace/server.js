6c5a4be8d420370feab4ef5b7a4a6c0ec016104b
Kunal Narkhede <152319973+Kunal-77@users.noreply.github.com>
2026-08-10 16:03:23 +0530

    Delete BCA_Q09_E/BCA_Q09_E/bca_api_routing directory

--- BCA_Q09_E/BCA_Q09_E/bca_api_routing/student_workspace/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-app.use(express.json());
-app.post('/api/users', (req, res) => {
-  if (!req.body.username) {
-    return res.status(400).json({ error: 'Username required' });
-  }
-  return res.status(201).json({ id: 1, username: req.body.username });
-});
