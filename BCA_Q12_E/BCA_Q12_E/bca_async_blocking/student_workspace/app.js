1e185bfe040b834486f3f1d1fec17845afa45784
Kunal Narkhede <152319973+Kunal-77@users.noreply.github.com>
2026-08-10 16:05:16 +0530

    Delete BCA_Q12_E/BCA_Q12_E/bca_async_blocking directory

--- BCA_Q12_E/BCA_Q12_E/bca_async_blocking/student_workspace/app.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs').promises;
-async function handleRequest(req, res) {
-  const data = await fs.readFile('data.json', 'utf-8');
-  return data;
-}
-module.exports = { handleRequest };
